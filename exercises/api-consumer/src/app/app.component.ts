d4272f72bd422dcae6b1b386bead6edd646840c7
Alex Cesar <thealexcesar@gmail.com>
2024-08-15 15:48:15 -0300

    Home + style

--- exercises/api-consumer/src/app/app.component.ts
+++ exercises/api-consumer/src/app/app.component.ts
@@ -2,12 +2,14 @@ import {Component} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
 import {FaIconLibrary, FontAwesomeModule} from "@fortawesome/angular-fontawesome";
 import {fas} from "@fortawesome/free-solid-svg-icons";
+import {HomeComponent} from "./components/home/home.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     imports: [
         RouterOutlet,
+        HomeComponent,
         FontAwesomeModule
     ],
     templateUrl: './app.component.html',
